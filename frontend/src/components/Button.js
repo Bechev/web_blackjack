b91cc5a0613e68c3bed91152269cb1d83e29b657
BCT <bertrand.chevalier@gmail.com>
2019-02-16 21:03:03 +0000

    first commit

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,24 @@
+import React, { Component }  from 'react'
+import './blackjack.css'
+
+class Button extends Component {
+
+    handleClick = () => {
+        if (this.props.isActive){
+            this.props.handleClick()
+        }
+    }
+    
+    render() {
+        let buttonClass = 'button';
+        if (this.props.isActive) {
+            buttonClass += ' active_button';
+        }
+        
+    return (
+        <button className={buttonClass}  onClick={this.handleClick}>{this.props.name}</button>
+    );
+  }
+}
+    
+export default Button;
